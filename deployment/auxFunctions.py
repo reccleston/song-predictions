d918e8dba8a4112d325431b4b334fe1a38318341
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-05-19 21:57:50 -0400

    re-adding funct

--- deployment/auxFunctions.py
+++ deployment/auxFunctions.py
@@ -16,17 +16,18 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(cl
 def get_features(song):
     return spotify.audio_features(spotify.search(q=song, limit=1)['tracks']['items'][0]['uri'])[0]
 
-# def getInfo(song, SongList):
-#     print('this function runs: ', '\n', song)
-#     for m in SongList:
-#         print(m[0])
-#     for row in SongList:
-#         print('we made it')
-#         print(row)
-#         if row[0] == song:
-#             print('MATCHED @%@%@%@%@%@%@')
-#             return row[2:]
-#     # fromt he billboard 
+# extract info from billboard list
+def getInfo(song, SongList):
+    print('into massage data')
+    print(song)
+    for row in SongList:
+        # print('inside for loop')
+        for k in row.keys():
+            if row[k] == song:
+                return_entry = {k: row[k] for k in row if k not in ['song']}
+                # print(row)
+                # print(return_entry)
+                return return_entry
 
 def makeTestPoint(features):
     
