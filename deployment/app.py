1d15b56404aa4ebf4080ad677fe07ebf2433e2e5
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-05-22 01:43:16 -0400

    connecting pages edits

--- deployment/app.py
+++ deployment/app.py
@@ -30,14 +30,12 @@ model = pickle.load(open('models/LogRegW2021.sav','rb'))
 
 app = Flask(__name__)
 
-# query for all songs info
+# query for all billboard songs info
 TopSongs = connection.execute("""SELECT * FROM songs;""")
 
-# lists to hold song info from DB
+# lists to hold song info from billboard DB
 song_titles = []
-song_list = []
-data = []
-
+bbData = []
 # iterating over return from DB and appending to dict
 for row in TopSongs:
     my_dict = {
@@ -63,17 +61,84 @@ for row in TopSongs:
         "duratin_ms": row[19],
         "time_signature": row[20]
     }
-    data.append(my_dict)
+    bbData.append(my_dict)
     song_titles.append(row[0])
 
-data.append({'columns': ["song", "performer", "chart_position", "previous_position", "peak", "weeks_on_chart", "hitTF", "id", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duratin_ms", "time_signature"]})
+bbData.append({'columns': ["song", "performer", "chart_position", "previous_position", "peak", "weeks_on_chart", "hitTF", "id", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duratin_ms", "time_signature"]})
 
-# home route
+# the algorithm route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# song append route (is this necess?)
+# the tracks page
+@app.route('/tracks')
+def tracks():
+    return render_template("tracks.html")
+
+# route to display the sunburst chart
+@app.route('/sunburstbubble')
+def sunbubblevalues():
+    result = connection.execute("""SELECT * FROM data_cleaned;""")
+    print(result)
+    data = []
+    for row in result:
+        my_dict = {
+            "title":row[0],
+            "artist": row[1],
+            "genre": row[2],
+            "genre_num": row[3],
+            "subgenre": row[4],
+            "year": row[5],
+            "bpm": row[6],
+            "nrgy": row[7],
+            "dnce": row[8],
+            "dB": row[9],
+            "live": row[10],
+            "val": row[11],
+            "dur": row[12],
+            "acous": row[13],
+            "spch": row[14],
+            "pop": row[15]
+        }
+        data.append(my_dict)
+        print(row)
+    data.append({'columns': ["title", "artist", "genre", "genre_num", "subgenre", "year", "bpm", "nrgy", "dnce", "dB", "live", "val", "dur", "acous", "spch", "pop"]})
+    return(jsonify(data))
+
+# route to display heatmap
+@app.route('/heatmap')
+def heatmapvalues():
+    result = connection.execute("""SELECT * FROM corr_heatmap_vals;""")
+    data = []
+    for row in result:
+        my_dict = {
+            "feat1":row[0],
+            "feat2": row[1],
+            "vals": row[2]
+        }
+        data.append(my_dict)
+        print(row)
+    return(jsonify(data))
+
+# route to display bar graph
+@app.route('/bar')
+def barvalues():
+    result = connection.execute("""SELECT * FROM year_table;""")
+    data = []
+    for row in result:
+        my_dict = {
+            "year":row[0],
+            "nrgy": row[1],
+            "dnce": row[2],
+            "val": row[3],
+            "pop": row[4]
+        }
+        data.append(my_dict)
+        print(row)
+    return(jsonify(data))
+
+# route to create a songs list - might not be necessary 
 @app.route('/billboard_songs')
 def songs():
     for song in TopSongs:
@@ -81,49 +146,39 @@ def songs():
 
     return jsonify(song_titles)
 
-# prediction route
+# route to obtain and display predictions
 @app.route('/predict', methods=['POST'])
 def predict():
-    # test_features = get_features()
-
-    # pred = np.nan
+    
     predictText = ''
-    # compare user input to song titles
     for users_input_song in request.form.values():
-        # if the input is in the titles
         if users_input_song in song_titles:
             print(users_input_song)
-            # send to getInfo funct to get the data
-            song_info = getInfo(users_input_song, data)
-            # run the song info through the model
+            song_info = getInfo(users_input_song, bbData)
+            # for m in song_info:
             prediction = model.predict(song_info)
             print('LOOK HERE (from bb) -->', prediction)
-            # if the prediction is 'not a hit', send 'no hit' message
             if prediction == 0:
                 predictText = f'{users_input_song} is likely to not be a hit!'
                 return render_template('index.html', text = predictText)
-            # if prediction is 'hit', send 'hit' message
-            elif prediction == 1:
-                predictText = f'{users_input_song} is likely to be a hit!'
-                return render_template('index.html', text = predictText)
-        # if song is not in titles
-        else:
-            print(users_input_song)
-            # send to get_features funct with Spotify API
-            features = get_features(users_input_song)
-            # send to makeNewPoint funct to append zeros
-            new_pt = makeNewPoint(features)
-            # run the song through the model
-            prediction = model.predict(new_pt)
-            # if the prediction is 'not a hit', send 'no hit' message
-            if prediction == 0:
-                predictText = f'{users_input_song} is likely to not be a hit!'
-                return render_template('index.html', text = predictText)
-            # if prediction is 'hit', send 'hit' message
             elif prediction == 1:
                 predictText = f'{users_input_song} is likely to be a hit!'
                 return render_template('index.html', text = predictText)
 
+        else:
+            print(users_input_song)
+            features = get_features(users_input_song)
+            new_pt = makeTestPoint(features)
+            prediction = model.predict(new_pt)
+
+            print('LOOK HERE -->', prediction)
+
+            if prediction == 0:
+                predictText = f'{users_input_song} is likely to not be a hit!'
+                return render_template('index.html', text = predictText)
+            elif prediction == 1:
+                predictText = f'{users_input_song} is likely to be a hit!'
+                return render_template('index.html', text = predictText)
 
 if __name__ == "__main__":
     app.run(debug=True)
