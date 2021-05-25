ac28bb52f1c4aa3a2a65400eb94b784161fa542c
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-05-25 15:13:44 -0400

    schema updates

--- schema/billboard_schema.sql
+++ schema/billboard_schema.sql
@@ -25,3 +25,27 @@ create table top_songs (
 create table dates (
     chart_week date
 );
+
+create table latest_data (
+    song varchar,
+    performer varchar,
+    chart_position float,
+    previous_position float,
+    peak float,
+    weeks_on_chart float,
+    hitTF float,
+    id varchar,
+    danceability float,
+    energy float,
+    key float,
+    loudness float,
+    mode float,
+    speechiness float,
+    acousticness float,
+    instrumentalness float,
+    liveness float,
+    valence float,
+    tempo float,
+    duratin_ms float,
+    time_signature float
+);
